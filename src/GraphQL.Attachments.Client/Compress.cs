e7ef20bdcf9653bf41d53456e066bd599ccac844
Simon Cropp <simon.cropp@gmail.com>
2021-06-30 14:24:06 +1000

    Update Compress.cs

--- src/GraphQL.Attachments.Client/Compress.cs
+++ src/GraphQL.Attachments.Client/Compress.cs
@@ -4,7 +4,6 @@ static class Compress
 {
     public static string Query(string query)
     {
-        Guard.AgainstNullWhiteSpace(nameof(query), query);
         query = Regex.Replace(query, @"\s+", " ");
         return Regex.Replace(query, @"\s*(\[|\]|\{|\}|\(|\)|:|\,)\s*", "$1");
     }
