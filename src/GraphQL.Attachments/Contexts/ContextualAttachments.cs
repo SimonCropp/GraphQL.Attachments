3b22f92fabaddfd318ab63f509a419633ecc6e0d
Simon Cropp <simon.cropp@gmail.com>
2023-01-11 12:48:26 +1100

    add singlular IncomingAttachment to ContextualAttachments (#312)

--- src/GraphQL.Attachments/Contexts/ContextualAttachments.cs
+++ src/GraphQL.Attachments/Contexts/ContextualAttachments.cs
@@ -5,12 +5,18 @@ public static class ContextualAttachments
     public static IIncomingAttachments IncomingAttachments<TSource>(this IResolveFieldContext<TSource> context) =>
         context.GetAttachmentContext().Incoming;
 
+    public static AttachmentStream IncomingAttachment<TSource>(this IResolveFieldContext<TSource> context) =>
+        context.IncomingAttachments().Single().Value;
+
+    public static IIncomingAttachments IncomingAttachments(this IResolveFieldContext context) =>
+        context.GetAttachmentContext().Incoming;
+
+    public static AttachmentStream IncomingAttachment(this IResolveFieldContext context) =>
+        context.IncomingAttachments().Single().Value;
+
     public static IOutgoingAttachments OutgoingAttachments<TSource>(this IResolveFieldContext<TSource> context) =>
         context.GetAttachmentContext().Outgoing;
 
     public static IOutgoingAttachments OutgoingAttachments(this IResolveFieldContext context) =>
         context.GetAttachmentContext().Outgoing;
-
-    public static IIncomingAttachments IncomingAttachments(this IResolveFieldContext context) =>
-        context.GetAttachmentContext().Incoming;
 }
