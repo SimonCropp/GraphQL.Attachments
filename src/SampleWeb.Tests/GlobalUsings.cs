c89fae57fd799fd3a68224371840331a0330885d
Simon Cropp <simon.cropp@gmail.com>
2024-04-02 22:37:53 +1100

    refs and cleanup

--- src/SampleWeb.Tests/GlobalUsings.cs
+++ src/SampleWeb.Tests/GlobalUsings.cs
@@ -1,6 +1,4 @@
-﻿// Global using directives
-
-global using GraphQL;
+﻿global using GraphQL;
 global using GraphQL.Attachments;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.TestHost;
