8bd782fde545bdc98743622a5a793f979fb4b9f6
Simon Cropp <simon.cropp@gmail.com>
2025-09-11 14:37:25 +1000

    add IncomingAttachment by name

--- src/GraphQL.Attachments/Contexts/ContextualAttachments.cs
+++ src/GraphQL.Attachments/Contexts/ContextualAttachments.cs
@@ -8,11 +8,27 @@ public static class ContextualAttachments
     public static AttachmentStream IncomingAttachment<TSource>(this IResolveFieldContext<TSource> context) =>
         context.IncomingAttachments().Single().Value;
 
+    public static AttachmentStream IncomingAttachment<TSource>(this IResolveFieldContext<TSource> context, string name) =>
+        FindSingle(name, context.IncomingAttachments());
+
     public static IIncomingAttachments IncomingAttachments(this IResolveFieldContext context) =>
         context.GetAttachmentContext().Incoming;
 
     public static AttachmentStream IncomingAttachment(this IResolveFieldContext context) =>
         context.IncomingAttachments().Single().Value;
+
+    public static AttachmentStream IncomingAttachment(this IResolveFieldContext context, string name) =>
+        FindSingle(name, context.IncomingAttachments());
+
+    static AttachmentStream FindSingle(string name, IIncomingAttachments attachments)
+    {
+        if (attachments.TryGetValue(name, out var attachment))
+        {
+            return attachment;
+        }
+
+        throw new($"Attachment not found: '{name}'");
+    }
 
     public static IOutgoingAttachments OutgoingAttachments<TSource>(this IResolveFieldContext<TSource> context) =>
         context.GetAttachmentContext().Outgoing;
